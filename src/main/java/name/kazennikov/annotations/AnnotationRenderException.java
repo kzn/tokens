cdb04dc94be71e135bdf955f94dc8e766948e88c
Anton Kazennikov <kazennikov@gmail.com>
2012-09-09 23:04:54 +0400

    adding exception class

---
+++ src/main/java/name/kazennikov/annotations/AnnotationRenderException.java
@@ -0,0 +1,32 @@
+package name.kazennikov.annotations;
+
+public class AnnotationRenderException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AnnotationRenderException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnnotationRenderException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnnotationRenderException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AnnotationRenderException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
