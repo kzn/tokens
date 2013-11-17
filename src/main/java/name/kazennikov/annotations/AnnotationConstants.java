6cc9089560fdf54d976d1a6b0ae948bc9980e41d
Anton Kazennikov <kazennikov@gmail.com>
2013-11-17 19:17:53 +0400

    annotation constants file

---
+++ src/main/java/name/kazennikov/annotations/AnnotationConstants.java
@@ -0,0 +1,15 @@
+package name.kazennikov.annotations;
+
+public interface AnnotationConstants {
+	public static final String TOKEN = "Token";
+    public static final String WORD = "Word";
+    public static final String SENT = "Sentence";
+    public static final String DOC = "Document";
+	public static final String LOOKUP = "Lookup";
+    
+    public static final String TYPE = "type";
+    public static final String KIND = "kind";
+    public static final String DATA = "data";
+
+
+}
