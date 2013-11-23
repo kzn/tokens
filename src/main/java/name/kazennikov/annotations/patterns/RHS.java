09e27545a50e283776b997a8721636dc57181b73
Anton Kazennikov <kazennikov@gmail.com>
2013-11-23 22:41:41 +0400

    added javadoc

--- src/main/java/name/kazennikov/annotations/patterns/RHS.java
+++ src/main/java/name/kazennikov/annotations/patterns/RHS.java
@@ -5,8 +5,24 @@ import java.util.Map;
 import name.kazennikov.annotations.AnnotationList;
 import name.kazennikov.annotations.Document;
 
+/**
+ * Right Hand Side of a JAPE rule. 
+ * Mostly for side-effect actions on the matched annotation list
+ * 
+ * @author Anton Kazennikov
+ *
+ */
 public interface RHS {
 	public final static EmptyRHS EMPTY = new EmptyRHS();
 	
+	/**
+	 * Executes the actions of the matched document
+	 * 
+	 * @param doc processed document
+	 * @param input input annotation list
+	 * @param bindings found bindings
+	 * 
+	 * @return true on successful RHS execution
+	 */
 	public boolean execute(Document doc, AnnotationList input, Map<String, AnnotationList> bindings);
 }
